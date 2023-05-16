4b7107fa57767e16ef700f7ecc140fc0bff3a3fc
NguyenMinhChau <nmchauhcmue@gmail.com>
2023-05-16 17:50:25 +0700

    VINJOB - Change slider image contact

--- src/layouts/Contact/Contact.js
+++ src/layouts/Contact/Contact.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import className from 'classnames/bind';
 import styles from './Contact.module.css';
 import { SkeletonCP, SliderHeader } from '../../components';
-import LOGO_SLIDER_HEADER from '../../assets/images/contact_page_bgc.gif';
+import LOGO_SLIDER_HEADER from '../../assets/images/contact_page_bgc.png';
 
 const cx = className.bind(styles);
 
@@ -14,9 +14,7 @@ export default function Contact() {
 	return (
 		<div className={`${cx('container')}`}>
 			<SliderHeader
-				urlImage={
-					'https://images.unsplash.com/photo-1504670073073-6123e39e0754?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJlY3J1aXRtZW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=2000&q=2000'
-				}
+				urlImage={LOGO_SLIDER_HEADER}
 				title={`<div><b>HOTLINE: <a href='tel:0345335422' class='text-primary'>0345.335.422</a></b></div>
 						<div><b>EMAIL: <a href='mailto:support@vinjob.com.vn' class='text-primary'>support@vinjob.com.vn</a></b></div>`}
 				animateName="animate__fadeInBottomRight"
